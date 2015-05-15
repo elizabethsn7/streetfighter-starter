201726a478a2f569aec2341dd884a98d3f8ac64c
Elizabeth Kipp <liz@elizabethkipp.com>
2015-05-15 14:34:40 -0500

    java

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log("ready!");
+    
+    $(".ryu").mouseover(function() {
+    	$(".ryu-still").hide();
+    	$(".ryu-ready").show();
+    	
+    	console.log("mouseover");
+    });
+
+    $(".ryu").mouseout(function() {
+    	$(".ryu-still").show();
+    	$(".ryu-ready").hide();
+    	
+    	console.log("mouseout");
+    });
 });
